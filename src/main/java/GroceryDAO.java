8034777dfd2299ff3513f2e04f5e05864f079e97
shanthini2963-IT <shanthiniramanjaneya@gmail.com>
2025-04-18 20:53:01 +0530

    mini project 7

--- src/main/java/GroceryDAO.java
+++ src/main/java/GroceryDAO.java
@@ -28,7 +28,7 @@ public class GroceryDAO {
         List<String> groceries = new ArrayList<>();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "SELECT grocery_name FROM Grocery";
             PreparedStatement ps = connection.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
             while(rs.next()){
@@ -60,9 +60,9 @@ public class GroceryDAO {
         Connection connection = ConnectionUtil.getConnection();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "INSERT INTO Grocery (grocery_name) VALUES (?)";
             PreparedStatement ps = connection.prepareStatement(sql);
-
+            ps.setString(1, groceryName);
             //add code that leverages ps.setString here
 
             ps.executeUpdate();
